e0ffd941a5f071b53cd2f346ce3cbd4e852bbdbc
moonk1212 <zm8208@naver.com>
2020-03-21 02:51:59 +0900

    미완성

--- js/maskcrawl1.js
+++ js/maskcrawl1.js
@@ -14,17 +14,17 @@ getHtml()
   .then(html => {
     let ulList = [];
     const $ = cheerio.load(html.data);
-    const $bodyList = $("div.selectbox-list ul");
+    const data = $("div.selectbox-list ul").find('li.selectbox-item').text();
 
-    $bodyList.each(function(i, elem) {
+    /*$bodyList.each(function(i, elem) {
       ulList[i] = {
           mask1: $(this).find('li.selectbox-item').text(),
          mask2: $(this).find('li.selectbox-item').text(),
          mask3: $(this).find('li.selectbox-item').text()
       };
     });
-
-    const data = ulList.filter(n => n.mask1);
+    */
+    //const data = ulList.filter(n => n.mask1);
     return data;
   })
   .then(res => log(res));
