ba93c424a9de4b36048fcad3b169b8792978ccc4
moonk1212 <zm8208@naver.com>
2020-03-21 02:43:50 +0900

    npm설치와 크롤러 만들기

---
+++ js/maskcrawl1.js
@@ -0,0 +1,30 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const log = console.log;
+
+const getHtml = async () => {
+  try {
+    return await axios.get("https://smartstore.naver.com/aer-shop/products/4722827602");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+getHtml()
+  .then(html => {
+    let ulList = [];
+    const $ = cheerio.load(html.data);
+    const $bodyList = $("div.selectbox-list ul");
+
+    $bodyList.each(function(i, elem) {
+      ulList[i] = {
+          mask1: $(this).find('li.selectbox-item').text(),
+         mask2: $(this).find('li.selectbox-item').text(),
+         mask3: $(this).find('li.selectbox-item').text()
+      };
+    });
+
+    const data = ulList.filter(n => n.mask1);
+    return data;
+  })
+  .then(res => log(res));
