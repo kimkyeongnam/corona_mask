271e5b6a8af29bd1c191a6cdcc8fb33b5a422263
kimkyeongnam <kkyy0126@naver.com>
2020-04-12 17:01:44 +0900

    [Modify] connect index<->mask_search
    index에서 원하는 위치 입력 또는 버튼 클릭 시
    mask_search.html로 해당 좌표로 이동하도록 만들었습니다.

--- js/main.js
+++ js/main.js
@@ -1,32 +1,32 @@
 /***** Check the mask day of week *****/
 
-function chk_buy_mask(){
+function chk_buy_mask() {
     let date = new Date();
     let day = date.getDay();
 
     // Monday
-    if(day == 1){
+    if (day == 1) {
         document.getElementById("chk_buy").innerText = "오늘 마스크 구매 가능자는 '끝자리 0, 5년 출생자' 입니다.";
     }
 
     // Tuesday
-    else if(day == 2){
+    else if (day == 2) {
         document.getElementById("chk_buy").innerText = "오늘 마스크 구매 가능자는 '끝자리 1, 6년 출생자' 입니다.";
     }
     // Wednesday
-    else if(day == 3){
+    else if (day == 3) {
         document.getElementById("chk_buy").innerText = "오늘 마스크 구매 가능자는 '끝자리 2, 7년 출생자' 입니다.";
     }
     // Thursday
-    else if(day == 4){
+    else if (day == 4) {
         document.getElementById("chk_buy").innerText = "오늘 마스크 구매 가능자는 '끝자리 3, 8년 출생자' 입니다.";
     }
     // Friday
-    else if(day == 5){
+    else if (day == 5) {
         document.getElementById("chk_buy").innerText = "오늘 마스크 구매 가능자는 '끝자리 4, 9년 출생자' 입니다.";
     }
     // Satureday & Sunday
-    else if(day == 6 || day == 0){
+    else if (day == 6 || day == 0) {
         document.getElementById("chk_buy").innerText = "오늘은 자유롭게 마스크를 구매할 수 있습니다.";
     }
 }
@@ -35,24 +35,34 @@ function chk_buy_mask(){
 
 
 /***** Page load: search & map page load *****/
-function search(select_value){
-    let mylocation;
-
-    //button click: current location search
-    if(select_value == 1){ 
-        // get current location value
-        mylocation = [33.450701, 126.570667];
-    }
-
+function search(select_value) {
+    let mylocation; // init setting
 
     // bar search: specific area
-    else{
+    if (select_value == 'bar'){
         // !!!parsing!!! : find location value
-        mylocation = [33.33412, 125.44323]; //temp setting
-
+        mylocation = [37.55998, 126.9858296]; //temp setting //명동
+        
+        // road map page
+        location.href = "./mask_search.html" + '#' + mylocation;
     }
 
-
-    // road map page
-    location.href = "./mask_search.html" + '#' + mylocation;
+    //button click: current location search
+    else if (select_value == 'button') {
+        // success getting current location
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                mylocation = [position.coords.latitude, position.coords.longitude];
+                
+                // road map page
+                location.href = "./mask_search.html" + '#' + mylocation;
+            });
+        } 
+        // fail getting current location
+        else {
+            message = 'geolocation을 사용할수 없어요..'
+            mylocation = [37.54404, 127.09637]; // 광진구 구의동
+            displayMarker(locPosition, message);
+        }
+    }
 }
--- js/map.js
+++ js/map.js
@@ -3,9 +3,11 @@ var mylocation; //[위도, 경도]
 
 function get_prev_info() {
     let entire_mylocation = window.location.hash.substring(1);
-    mylocation = entire_mylocation.split(',')
-    console.log(mylocation[0])
+    console.log(entire_mylocation);
+    mylocation = entire_mylocation.split(',');
+    console.log(mylocation[0]);
     console.log(mylocation[1]);
+    searchMyPos();
 }
 get_prev_info();
 
@@ -84,30 +86,30 @@ function searchMyPos() {
 
     var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
-    // HTML5의 geolocation으로 사용할 수 있는지 확인합니다 
-    if (navigator.geolocation) {
+    // // HTML5의 geolocation으로 사용할 수 있는지 확인합니다 
+    // if (navigator.geolocation) {
 
-        // GeoLocation을 이용해서 접속 위치를 얻어옵니다
-        navigator.geolocation.getCurrentPosition(function (position) {
+    //     // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+    //     navigator.geolocation.getCurrentPosition(function (position) {
 
-            var lat = position.coords.latitude, // 위도
-                lon = position.coords.longitude; // 경도
+    //         var lat = position.coords.latitude, // 위도
+    //             lon = position.coords.longitude; // 경도
 
-            var locPosition = new kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
-                message = '<div style="padding:5px;">여기에 계신가요?!</div>'; // 인포윈도우에 표시될 내용입니다
+    //         var locPosition = new kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
+    //             message = '<div style="padding:5px;">여기에 계신가요?!</div>'; // 인포윈도우에 표시될 내용입니다
 
-            // 마커와 인포윈도우를 표시합니다
-            displayMarker(locPosition, message);
+    //         // 마커와 인포윈도우를 표시합니다
+    //         displayMarker(locPosition, message);
 
-        });
+    //     });
 
-    } else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
+    // } else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
 
-        var locPosition = new kakao.maps.LatLng(33.450701, 126.570667),
-            message = 'geolocation을 사용할수 없어요..'
+    //     var locPosition = new kakao.maps.LatLng(mylocation[0], mylocation[1]),
+    //         message = 'geolocation을 사용할수 없어요..'
 
-        displayMarker(locPosition, message);
-    }
+    //     displayMarker(locPosition, message);
+    // }
 
     // 지도에 마커와 인포윈도우를 표시하는 함수입니다
     function displayMarker(locPosition, message) {
